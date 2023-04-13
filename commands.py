8e8eb18ae5418f24b69a53c470a461077021ceb9
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-04-13 23:40:52 +0300

    #2

--- commands.py
+++ commands.py
@@ -1,7 +1,21 @@
 from inventory.models import Brand
+from django.db import connection, reset_queries
 
 Brand.objects.create(brand_id=1, name="adidas")  # Создает объект
 
 x = Brand.objects.all()  # ВОзвращает кверисет из данных
 
 x = Brand.objects.all().query  # используется к кверисет чтобы увидеть запрос СКЬЮЭЛь
+
+connection.queries #Посмотреть все последние запросы скьэль
+reset_queries() #очистить все последние запросы скьэль
+
+x = Brand.objects.filter(brand_id=1) #Фильтр по определенным параматрам
+x = Brand.objects.all().count() #Подсчет вернувшихся объектов
+len(x) #Подсчет вернувшихся объектов
+
+for i in x:
+    print(i.brand_id)
+
+Brand.objects.all().values() #Возвращат данные в виде словаря
+Brand.objects.all().values('name') #Возвращат данные в виде словаря по определенным параматрам
