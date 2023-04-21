3810b9010e966ebb5f1f0e19971a17fd9be1065b
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-04-21 16:00:59 +0300

    #10 filter | exclude

--- commnads_retrieve.py
+++ commnads_retrieve.py
@@ -1,6 +1,16 @@
 from inventory.models import Tag
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 
 Tag.objects.get(id=1) #Получение только одного объекта
 Tag.objects.get_or_create(name="django-react") #Получение одного объекта, при отсутсвии создание его
 get_object_or_404(Tag, name="django-react") #Получение одного объекта, при отсутсвии ошибка 404
+
+Tag.objects.all().filter(id=4) #Фильтр по айди
+Tag.objects.filter(id=4)#Фильтр по айди
+Tag.objects.exclude(id=3)#Фильтр исключить по айди
+
+Tag.objects.filter(name="django") | Tag.objects.filter( name="react")#Фильтр используя ИЛИ
+Tag.objects.filter(Q(id=2) | Q(id=5))#Фильтр используя ИЛИ
+Tag.objects.filter(id__gt=2)#Фильтр по ГОТОВЫМ ПАРАМЕТРАМ (Больше чем)
+Tag.objects.filter(name__startswith="d")#Фильтр по ГОТОВЫМ ПАРАМЕТРАМ (начинается с)
